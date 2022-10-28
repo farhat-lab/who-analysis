1e83ba35d7e0f6945d9709b3ee5bba479f5a6f4c
Sanjana <sgkulkarni02@gmail.com>
2022-10-28 16:25:48 -0400

    p value thresholds

--- 04_combine_model_analyses.py
+++ 04_combine_model_analyses.py
@@ -16,6 +16,11 @@ _, drug = sys.argv
 # get the core model for the drug: tier 1 only, WHO phenos only, no synonymous mutations, pool LOF (if it's not different from unpool LOF, use the unpooled model)
 out_dir = '/n/data1/hms/dbmi/farhat/ye12/who/analysis'
 
+# remove any file in the directory (that's not a directory itself) so that it does not appear in the lists later in the nested for loop
+files = glob.glob(os.path.join(out_dir, "*.csv")
+for f in files:
+    os.remove(f)
+
 core_out_dir = os.path.join(out_dir, drug, "tiers=1/phenos=WHO")
 core_model_path = os.path.join(core_out_dir, "dropAF_noSyn_poolLOF")
 
@@ -28,6 +33,9 @@ core_analysis["Tier1_only"] = 1
 core_analysis["WHO_phenos"] = 1
 core_analysis["poolLOF"] = 1
 core_analysis["Syn"] = 0
+
+# keep all significant variants at an FDR threshold of 0.05
+core_analysis = core_analysis.loc[core_analysis["BH_pval"] < 0.05]
 
 # create dictionary of the additional model analyses
 add_analyses = {}
@@ -49,7 +57,9 @@ for tier in os.listdir(os.path.join(out_dir, drug)):
                     add_analysis["WHO_phenos"] = int("WHO" in add_path)
                     add_analysis["poolLOF"] = int("LOF" in add_path)
                     add_analysis["Syn"] = int("withSyn" in add_path)
-                    add_analyses[add_path] = add_analysis
+                    
+                    # keep all significant variants at an FDR threshold of 0.01
+                    add_analyses[add_path] = add_analysis.loc[add_analysis["BH_pval"] < 0.01]
             else:
                 print(f"No model analysis file in {analysis_fName}")
                 
@@ -67,7 +77,9 @@ merged_df = pd.concat([core_analysis, add_analyses[keys_lst[0]]], axis=0).drop_d
 for key in keys_lst[1:]:
     merged_df = pd.concat([core_analysis, add_analyses[key]], axis=0).drop_duplicates("orig_variant", keep="first")
     
-# keep variants with an FDR-corrected p-value < 0.05 for tier1 genes and < 0.01 for tier1+2 models. 
-merged_significant = merged_df.loc[((merged_df["Tier1_only"] == 1) & (merged_df["BH_pval"] < 0.05)) |
-                                   ((merged_df["Tier1_only"] == 0) & (merged_df["BH_pval"] < 0.01))
-                                  ]
+# # keep variants with an FDR-corrected p-value < 0.05 for the core model.  
+# merged_significant = merged_df.loc[((merged_df["Tier1_only"] == 1) & (merged_df["WHO_phenos"] == 1) & (merged_df["poolLOF"] == 1) & (merged_df["Syn"] == 0) & (merged_df["BH_pval"] < 0.05)) |
+#                                    ((merged_df["Tier1_only"] == 0) & (merged_df["BH_pval"] < 0.01))
+#                                   ]
+
+merged_df.to_csv(os.path.join(os.path.join(out_dir, drug, "final_analysis.csv")), index=False)
