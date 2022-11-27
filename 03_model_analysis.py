cfa387d32ee59fddb43fc1cfd80bc0d824c2f71d
Sanjana <sgkulkarni02@gmail.com>
2022-11-27 01:38:19 -0500

    sanity checks for confidence intervals

--- 03_model_analysis.py
+++ 03_model_analysis.py
@@ -46,58 +46,6 @@ def get_pvalues_add_ci(coef_df, bootstrap_df, col, num_samples, tier1_variants=N
         
 
 
-# aa_code_dict = {'VAL':'V', 'ILE':'I', 'LEU':'L', 'GLU':'E', 'GLN':'Q', \
-# 'ASP':'D', 'ASN':'N', 'HIS':'H', 'TRP':'W', 'PHE':'F', 'TYR':'Y',    \
-# 'ARG':'R', 'LYS':'K', 'SER':'S', 'THR':'T', 'MET':'M', 'ALA':'A',    \
-# 'GLY':'G', 'PRO':'P', 'CYS':'C'}
-
-
-# def find_SNVs_in_current_WHO(coef_df, aa_code_dict, drug_abbr):
-#     '''
-#     This function currently only looks for SNVs in the 2021 version of the WHO mutation catalog. 
-#     TO-DO: Code up indel comparisons between the measured variants and the variants in the 2021 catalog.
-#     '''
-#     new_variants = []
-
-#     # need to change the naming convention of variants in order to compare strings with the current WHO mutation catalog
-#     for variant in coef_df.variant.values:
-
-#         new_variant = variant.split(".")[-1]
-        
-#         # nucleotide change conversions
-#         if ">" in new_variant:
-#             og_nuc = new_variant.split(">")[0][-1]
-#             mut_nuc = new_variant.split(">")[1]
-
-#             og_nuc_idx = list(new_variant).index(">")-1
-#             pos = int(new_variant[:og_nuc_idx])
-
-#             new_variant = f"{og_nuc.lower()}{pos}{mut_nuc.lower()}"
-            
-#         else:
-#             # AA conversions
-#             for key, value in aa_code_dict.items():
-
-#                 if key in new_variant.upper():
-#                     new_variant = new_variant.upper().replace(key, value)
-
-#         new_variants.append(variant.split("_")[0] + "_" + new_variant)
-        
-#     coef_df["gene"] = [variant.split("_")[0] for variant in coef_df.variant.values]
-#     coef_df.rename(columns={"variant": "orig_variant"}, inplace=True)
-#     coef_df["variant"] = new_variants
-    
-#     # WHO variants are named with the gene first, followed by an underscore, then the variant itself
-#     who_single_drug = who_variants.query("drug == @drug_abbr")
-#     coef_df = coef_df.merge(who_single_drug[["genome_index", "confidence", "variant"]], on="variant", how="outer")
-#     coef_df.rename(columns={"confidence": "confidence_WHO_2021"}, inplace=True)
-    
-#     # drop duplicates that arise from the same AA variant being associated with multiple nucleotide mutations in the 2021 catalog
-#     return coef_df.dropna(subset=["orig_variant"]).drop_duplicates(subset=["orig_variant"], keep='first').reset_index(drop=True)
-
-
-
-
 def BH_FDR_correction(coef_df):
     '''
     Implement Benjamini-Hochberg FDR correction.
@@ -126,7 +74,7 @@ def run_all(out_dir, drug, drug_abbr, **kwargs):
     
     # coefficients from L2 regularized regression ("baseline" regression)
     coef_df = pd.read_csv(os.path.join(out_dir, "regression_coef.csv"))
-    coef_df = coef_df.query("coef != 0")
+    #coef_df = coef_df.query("coef != 0")
 
     # coefficients from bootstrap replicates
     bs_df = pd.read_csv(os.path.join(out_dir, "coef_bootstrap.csv"))
@@ -149,20 +97,13 @@ def run_all(out_dir, drug, drug_abbr, **kwargs):
     assert len(coef_df.query("pval > BH_pval")) == 0
     assert len(coef_df.query("pval > Bonferroni_pval")) == 0
 
-#     # return all features with non-zero coefficients.
-#     coef_df = find_SNVs_in_current_WHO(coef_df, aa_code_dict, drug_abbr)
-
     # convert to odds ratios
     if binary:
         coef_df["Odds_Ratio"] = np.exp(coef_df["coef"])
         coef_df["OR_LB"] = np.exp(coef_df["coef_LB"])
         coef_df["OR_UB"] = np.exp(coef_df["coef_UB"])
- 
-    # clean up the dataframe a little -- variant and gene are from the 2021 catalog (redundant with the orig_variant column)
-    del coef_df["variant"]
-    del coef_df["gene"]
     
-    return coef_df.drop_duplicates("orig_variant", keep='first').sort_values("coef", ascending=False).reset_index(drop=True)
+    return coef_df.drop_duplicates("variant", keep='first').sort_values("coef", ascending=False).reset_index(drop=True)
 
 
 _, config_file, drug, drug_WHO_abbr = sys.argv
