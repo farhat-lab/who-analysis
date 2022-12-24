89e622fa4266847a24f6d7cc3da072fc50e9795a
Sanjana <sgkulkarni02@gmail.com>
2022-12-24 18:27:04 -0500

    removed duplicate Pretomanid analyses

--- 01_make_model_inputs.py
+++ 01_make_model_inputs.py
@@ -280,159 +280,159 @@ if not synonymous:
 ############# STEP 3: POOL LOF MUTATIONS, IF INDICATED BY THE MODEL PARAMS #############
 
 
-def pool_mutations(df, effect_lst, pool_col):
+# def pool_mutations(df, effect_lst, pool_col):
     
-    df.loc[df["predicted_effect"].isin(effect_lst), ["variant_category", "position"]] = [pool_col, np.nan]
+#     df.loc[df["predicted_effect"].isin(effect_lst), ["variant_category", "position"]] = [pool_col, np.nan]
 
-    # sort descending to keep the largest variant_binary_status and variant_allele_frequency first. In this way, pooled mutations that are actually present are preserved
-    df_pooled = df.query("variant_category == @pool_col").sort_values(by=["variant_binary_status", "variant_allele_frequency"], ascending=False, na_position="last").drop_duplicates(subset=["sample_id", "resolved_symbol"], keep="first")
+#     # sort descending to keep the largest variant_binary_status and variant_allele_frequency first. In this way, pooled mutations that are actually present are preserved
+#     df_pooled = df.query("variant_category == @pool_col").sort_values(by=["variant_binary_status", "variant_allele_frequency"], ascending=False, na_position="last").drop_duplicates(subset=["sample_id", "resolved_symbol"], keep="first")
 
-    # combine with the unpooled variants and the other variants and return
-    return pd.concat([df_pooled, df.query("variant_category != @pool_col")], axis=0)
-
-
-# options for pool_type are unpooled, poolSeparate, and poolALL
-if pool_type == "poolSeparate":
-    print("Pooling LOF and inframe mutations separately")
-    df_model = pool_mutations(df_model, ["frameshift", "start_lost", "stop_gained", "feature_ablation"], "lof")
-    df_model = pool_mutations(df_model, ["inframe_insertion", "inframe_deletion"], "inframe")
-
-elif pool_type == "poolALL":
-    print("Pooling LOF and inframe mutations together into a single feature")
-    df_model = pool_mutations(df_model, ["frameshift", "start_lost", "stop_gained", "feature_ablation", "inframe_insertion", "inframe_deletion"], "lof_all")
+#     # combine with the unpooled variants and the other variants and return
+#     return pd.concat([df_pooled, df.query("variant_category != @pool_col")], axis=0)
 
 
-############# STEP 4: PROCESS AMBIGUOUS ALLELES -- I.E. THOSE WITH 0.25 <= AF <= 0.75 #############
+# # options for pool_type are unpooled, poolSeparate, and poolALL
+# if pool_type == "poolSeparate":
+#     print("Pooling LOF and inframe mutations separately")
+#     df_model = pool_mutations(df_model, ["frameshift", "start_lost", "stop_gained", "feature_ablation"], "lof")
+#     df_model = pool_mutations(df_model, ["inframe_insertion", "inframe_deletion"], "inframe")
+
+# elif pool_type == "poolALL":
+#     print("Pooling LOF and inframe mutations together into a single feature")
+#     df_model = pool_mutations(df_model, ["frameshift", "start_lost", "stop_gained", "feature_ablation", "inframe_insertion", "inframe_deletion"], "lof_all")
 
 
-# set variants with AF <= the threshold as wild-type and AF > the threshold as alternative
-if amb_mode == "BINARY":
-    print(f"Binarizing ambiguous variants with AF threshold of {AF_thresh}")
-    df_model.loc[(pd.isnull(df_model["variant_binary_status"])) & (df_model["variant_allele_frequency"] <= AF_thresh), "variant_binary_status"] = 0
-    df_model.loc[(pd.isnull(df_model["variant_binary_status"])) & (df_model["variant_allele_frequency"] > AF_thresh), "variant_binary_status"] = 1
+# ############# STEP 4: PROCESS AMBIGUOUS ALLELES -- I.E. THOSE WITH 0.25 <= AF <= 0.75 #############
 
-# use ambiguous AF as the matrix value for variants with AF > 0.25. Below 0.25, the AF measurements aren't reliable
-elif amb_mode == "AF":
-    print("Encoding ambiguous variants with their AF")
-    # encode all variants with AF > 0.25 with their AF
-    df_model.loc[df_model["variant_allele_frequency"] > 0.25, "variant_binary_status"] = df_model.loc[df_model["variant_allele_frequency"] > 0.25, "variant_allele_frequency"].values
+
+# # set variants with AF <= the threshold as wild-type and AF > the threshold as alternative
+# if amb_mode == "BINARY":
+#     print(f"Binarizing ambiguous variants with AF threshold of {AF_thresh}")
+#     df_model.loc[(pd.isnull(df_model["variant_binary_status"])) & (df_model["variant_allele_frequency"] <= AF_thresh), "variant_binary_status"] = 0
+#     df_model.loc[(pd.isnull(df_model["variant_binary_status"])) & (df_model["variant_allele_frequency"] > AF_thresh), "variant_binary_status"] = 1
+
+# # use ambiguous AF as the matrix value for variants with AF > 0.25. Below 0.25, the AF measurements aren't reliable
+# elif amb_mode == "AF":
+#     print("Encoding ambiguous variants with their AF")
+#     # encode all variants with AF > 0.25 with their AF
+#     df_model.loc[df_model["variant_allele_frequency"] > 0.25, "variant_binary_status"] = df_model.loc[df_model["variant_allele_frequency"] > 0.25, "variant_allele_frequency"].values
    
-# drop all isolates with ambiguous variants with ANY AF below the threshold. DON'T DROP FEATURES BECAUSE MIGHT DROP SOMETHING RELEVANT
-elif amb_mode == "DROP":
-    drop_isolates = df_model.loc[(pd.isnull(df_model["variant_binary_status"])) & (df_model["variant_allele_frequency"] <= 0.75)].sample_id.unique()
-    print(f"Dropped {len(drop_isolates)} isolates with any intermediate AFs. Remainder are binary")
-    df_model = df_model.query("sample_id not in @drop_isolates")    
+# # drop all isolates with ambiguous variants with ANY AF below the threshold. DON'T DROP FEATURES BECAUSE MIGHT DROP SOMETHING RELEVANT
+# elif amb_mode == "DROP":
+#     drop_isolates = df_model.loc[(pd.isnull(df_model["variant_binary_status"])) & (df_model["variant_allele_frequency"] <= 0.75)].sample_id.unique()
+#     print(f"Dropped {len(drop_isolates)} isolates with any intermediate AFs. Remainder are binary")
+#     df_model = df_model.query("sample_id not in @drop_isolates")    
     
-# check after this step that the only NaNs left are truly missing data --> NaN in variant_binary_status must also be NaN in variant_allele_frequency
-assert len(df_model.loc[(~pd.isnull(df_model["variant_allele_frequency"])) & (pd.isnull(df_model["variant_binary_status"]))]) == 0
+# # check after this step that the only NaNs left are truly missing data --> NaN in variant_binary_status must also be NaN in variant_allele_frequency
+# assert len(df_model.loc[(~pd.isnull(df_model["variant_allele_frequency"])) & (pd.isnull(df_model["variant_binary_status"]))]) == 0
 
 
-############# STEP 5: PIVOT TO MATRIX AND DROP HIGH-FREQUENCY MISSINGNESS #############
+# ############# STEP 5: PIVOT TO MATRIX AND DROP HIGH-FREQUENCY MISSINGNESS #############
 
 
-# 1 = alternative allele, 0 = reference allele, NaN = missing
-df_model["mutation"] = df_model["resolved_symbol"] + "_" + df_model["variant_category"]
+# # 1 = alternative allele, 0 = reference allele, NaN = missing
+# df_model["mutation"] = df_model["resolved_symbol"] + "_" + df_model["variant_category"]
 
-# drop more duplicates, but I think this might be because we have multiple data pulls at a time
-# NaN is larger than any number, so sort ascending and keep first
-df_model = df_model.sort_values("variant_binary_status").drop_duplicates(["sample_id", "mutation"], keep="first")
+# # drop more duplicates, but I think this might be because we have multiple data pulls at a time
+# # NaN is larger than any number, so sort ascending and keep first
+# df_model = df_model.sort_values("variant_binary_status").drop_duplicates(["sample_id", "mutation"], keep="first")
 
-matrix = df_model.pivot(index="sample_id", columns="mutation", values="variant_binary_status")
+# matrix = df_model.pivot(index="sample_id", columns="mutation", values="variant_binary_status")
 
-# in this case, only 3 possible values -- 0 (ref), 1 (alt), NaN (missing)
-if amb_mode.upper() in ["BINARY", "DROP"]:
-    assert len(np.unique(matrix.values)) <= 3
-# the smallest value will be 0. Check that the second smallest value is greater than 0.25 (below this, AFs are not really reliable)
-else:
-    assert np.sort(np.unique(matrix.values))[1] > 0.25
+# # in this case, only 3 possible values -- 0 (ref), 1 (alt), NaN (missing)
+# if amb_mode.upper() in ["BINARY", "DROP"]:
+#     assert len(np.unique(matrix.values)) <= 3
+# # the smallest value will be 0. Check that the second smallest value is greater than 0.25 (below this, AFs are not really reliable)
+# else:
+#     assert np.sort(np.unique(matrix.values))[1] > 0.25
 
-# compare proportions of missing isolates or variants to determine which is larger and drop that first. usually, isolates have more missingness
-# maximum proportion of missing isolates per feature
-max_prop_missing_isolates_per_feature = matrix.isna().sum(axis=0).max() / matrix.shape[0]
-# maximum proportion of missing features per isolate
-max_prop_missing_variants_per_isolate = matrix.isna().sum(axis=1).max() / matrix.shape[1]
+# # compare proportions of missing isolates or variants to determine which is larger and drop that first. usually, isolates have more missingness
+# # maximum proportion of missing isolates per feature
+# max_prop_missing_isolates_per_feature = matrix.isna().sum(axis=0).max() / matrix.shape[0]
+# # maximum proportion of missing features per isolate
+# max_prop_missing_variants_per_isolate = matrix.isna().sum(axis=1).max() / matrix.shape[1]
 
-# drop isolates first if there are isolates with a lot of missingness (many variants missing)
-if max_prop_missing_variants_per_isolate >= max_prop_missing_isolates_per_feature:
-    print(f"    Up to {round(max_prop_missing_variants_per_isolate*100, 2)}% of variants per isolate and {round(max_prop_missing_isolates_per_feature*100, 2)}% of isolates per variant have missing data")
+# # drop isolates first if there are isolates with a lot of missingness (many variants missing)
+# if max_prop_missing_variants_per_isolate >= max_prop_missing_isolates_per_feature:
+#     print(f"    Up to {round(max_prop_missing_variants_per_isolate*100, 2)}% of variants per isolate and {round(max_prop_missing_isolates_per_feature*100, 2)}% of isolates per variant have missing data")
     
-    # drop isolates (rows) with missingness above the threshold (default = 1%)
-    filtered_matrix = matrix.dropna(axis=0, thresh=(1-missing_isolate_thresh)*matrix.shape[1])
+#     # drop isolates (rows) with missingness above the threshold (default = 1%)
+#     filtered_matrix = matrix.dropna(axis=0, thresh=(1-missing_isolate_thresh)*matrix.shape[1])
     
-    print(f"    Dropped {matrix.shape[0] - filtered_matrix.shape[0]}/{matrix.shape[0]} isolates with >{int(missing_isolate_thresh*100)}% missingness")
-    num_isolates_after_isolate_thresh = filtered_matrix.shape[0]
-    num_features_after_isolate_thresh = filtered_matrix.shape[1]
+#     print(f"    Dropped {matrix.shape[0] - filtered_matrix.shape[0]}/{matrix.shape[0]} isolates with >{int(missing_isolate_thresh*100)}% missingness")
+#     num_isolates_after_isolate_thresh = filtered_matrix.shape[0]
+#     num_features_after_isolate_thresh = filtered_matrix.shape[1]
     
-    # drop features (columns) with missingness above the threshold (default = 1%)
-    filtered_matrix = filtered_matrix.dropna(axis=1, thresh=(1-missing_feature_thresh)*filtered_matrix.shape[0])
-    print(f"    Dropped {num_features_after_isolate_thresh - filtered_matrix.shape[1]}/{num_features_after_isolate_thresh} variants with >{int(missing_feature_thresh*100)}% missingness")
-    num_isolates_after_thresholding = filtered_matrix.shape[0]
+#     # drop features (columns) with missingness above the threshold (default = 1%)
+#     filtered_matrix = filtered_matrix.dropna(axis=1, thresh=(1-missing_feature_thresh)*filtered_matrix.shape[0])
+#     print(f"    Dropped {num_features_after_isolate_thresh - filtered_matrix.shape[1]}/{num_features_after_isolate_thresh} variants with >{int(missing_feature_thresh*100)}% missingness")
+#     num_isolates_after_thresholding = filtered_matrix.shape[0]
     
-# drop variants first if there are variants with a lot of missingness (many isolates missing)
-else:
-    print(f"    Up to {round(max_prop_missing_isolates_per_feature*100, 2)}% of isolates per variant and {round(max_prop_missing_variants_per_isolate*100, 2)}% of variants per isolate have missing data")
+# # drop variants first if there are variants with a lot of missingness (many isolates missing)
+# else:
+#     print(f"    Up to {round(max_prop_missing_isolates_per_feature*100, 2)}% of isolates per variant and {round(max_prop_missing_variants_per_isolate*100, 2)}% of variants per isolate have missing data")
     
-    # drop features (columns) with missingness above the threshold (default = 1%)
-    filtered_matrix = matrix.dropna(axis=1, thresh=(1-missing_feature_thresh)*matrix.shape[0])
+#     # drop features (columns) with missingness above the threshold (default = 1%)
+#     filtered_matrix = matrix.dropna(axis=1, thresh=(1-missing_feature_thresh)*matrix.shape[0])
     
-    print(f"    Dropped {matrix.shape[1] - filtered_matrix.shape[1]}/{matrix.shape[1]} variants with >{int(missing_feature_thresh*100)}% missingness")
-    num_isolates_after_variant_thresh = filtered_matrix.shape[0]
-    num_variants_after_variant_thresh = filtered_matrix.shape[1]
+#     print(f"    Dropped {matrix.shape[1] - filtered_matrix.shape[1]}/{matrix.shape[1]} variants with >{int(missing_feature_thresh*100)}% missingness")
+#     num_isolates_after_variant_thresh = filtered_matrix.shape[0]
+#     num_variants_after_variant_thresh = filtered_matrix.shape[1]
 
-    # drop isolates (rows) with missingness above the threshold (default = 1%)
-    filtered_matrix = filtered_matrix.dropna(axis=0, thresh=(1-missing_isolate_thresh)*filtered_matrix.shape[1])
-    print(f"    Dropped {num_isolates_after_variant_thresh - filtered_matrix.shape[0]}/{num_isolates_after_variant_thresh} isolates with >{int(missing_isolate_thresh*100)}% missingness")
-    num_isolates_after_thresholding = filtered_matrix.shape[0]
-
-
-############# STEP 6: IMPUTE OR DROP REMAINING NANS IN THE GENOTYPES -- THESE ARE LOW FREQUENCY MISSING DATA #############
+#     # drop isolates (rows) with missingness above the threshold (default = 1%)
+#     filtered_matrix = filtered_matrix.dropna(axis=0, thresh=(1-missing_isolate_thresh)*filtered_matrix.shape[1])
+#     print(f"    Dropped {num_isolates_after_variant_thresh - filtered_matrix.shape[0]}/{num_isolates_after_variant_thresh} isolates with >{int(missing_isolate_thresh*100)}% missingness")
+#     num_isolates_after_thresholding = filtered_matrix.shape[0]
 
 
-# can only impute as this is written for binary phenotypes. Not going to impute anyway, so I'm not going to adapt this for MIC samples
-if impute and binary:
-    # use only samples that are in the filtered matrix for imputation
-    df_phenos = df_phenos.query("sample_id in @filtered_matrix.index.values")
+# ############# STEP 6: IMPUTE OR DROP REMAINING NANS IN THE GENOTYPES -- THESE ARE LOW FREQUENCY MISSING DATA #############
+
+
+# # can only impute as this is written for binary phenotypes. Not going to impute anyway, so I'm not going to adapt this for MIC samples
+# if impute and binary:
+#     # use only samples that are in the filtered matrix for imputation
+#     df_phenos = df_phenos.query("sample_id in @filtered_matrix.index.values")
     
-    impute_cols = filtered_matrix.columns[filtered_matrix.isna().any()]
-    print(f"    There are NaNs in {len(impute_cols)}/{filtered_matrix.shape[1]} genetic features")
+#     impute_cols = filtered_matrix.columns[filtered_matrix.isna().any()]
+#     print(f"    There are NaNs in {len(impute_cols)}/{filtered_matrix.shape[1]} genetic features")
 
-    if len(impute_cols) > 0:
-        print("Imputing missing data")
-        for i, col in enumerate(impute_cols):
+#     if len(impute_cols) > 0:
+#         print("Imputing missing data")
+#         for i, col in enumerate(impute_cols):
 
-            # isolates without a genotype for this column
-            missing_isolates = filtered_matrix.loc[pd.isnull(filtered_matrix[col]), :].index.values
+#             # isolates without a genotype for this column
+#             missing_isolates = filtered_matrix.loc[pd.isnull(filtered_matrix[col]), :].index.values
 
-            susc_isolates = df_phenos.query("phenotype == 0").sample_id.values
-            resist_isolates = df_phenos.query("phenotype == 1").sample_id.values
+#             susc_isolates = df_phenos.query("phenotype == 0").sample_id.values
+#             resist_isolates = df_phenos.query("phenotype == 1").sample_id.values
 
-            # take the average of the genotype across isolates of the same class
-            susc_impute = filtered_matrix.loc[filtered_matrix.index.isin(susc_isolates), col].dropna().mean()
-            resist_impute = filtered_matrix.loc[filtered_matrix.index.isin(resist_isolates), col].dropna().mean()
+#             # take the average of the genotype across isolates of the same class
+#             susc_impute = filtered_matrix.loc[filtered_matrix.index.isin(susc_isolates), col].dropna().mean()
+#             resist_impute = filtered_matrix.loc[filtered_matrix.index.isin(resist_isolates), col].dropna().mean()
 
-            for isolate in missing_isolates:
+#             for isolate in missing_isolates:
 
-                if df_phenos.query("sample_id == @isolate").phenotype.values[0] == 1:
-                    filtered_matrix.loc[isolate, col] = resist_impute
-                else:
-                    filtered_matrix.loc[isolate, col] = susc_impute
+#                 if df_phenos.query("sample_id == @isolate").phenotype.values[0] == 1:
+#                     filtered_matrix.loc[isolate, col] = resist_impute
+#                 else:
+#                     filtered_matrix.loc[isolate, col] = susc_impute
 
-# don't impute anything, simply drop all isolates (rows) with NaNs.
-else:
-    filtered_matrix.dropna(axis=0, inplace=True, how="any")
-    print(f"    Dropped {num_isolates_after_thresholding - filtered_matrix.shape[0]} isolates with any remaining missingness")
+# # don't impute anything, simply drop all isolates (rows) with NaNs.
+# else:
+#     filtered_matrix.dropna(axis=0, inplace=True, how="any")
+#     print(f"    Dropped {num_isolates_after_thresholding - filtered_matrix.shape[0]} isolates with any remaining missingness")
        
         
-# there should not be any more NaNs
-assert sum(pd.isnull(np.unique(filtered_matrix.values))) == 0
+# # there should not be any more NaNs
+# assert sum(pd.isnull(np.unique(filtered_matrix.values))) == 0
 
-# remove mutations that are 0 for all isolates (there is no signal, and the variant will have a coefficient of 0)
-filtered_matrix = filtered_matrix[filtered_matrix.columns[~((filtered_matrix == 0).all())]]
+# # remove mutations that are 0 for all isolates (there is no signal, and the variant will have a coefficient of 0)
+# filtered_matrix = filtered_matrix[filtered_matrix.columns[~((filtered_matrix == 0).all())]]
 
-print(f"    Kept {filtered_matrix.shape[0]} isolates and {filtered_matrix.shape[1]} genetic variants")
-filtered_matrix.to_pickle(os.path.join(out_dir, "filt_matrix.pkl"))
+# print(f"    Kept {filtered_matrix.shape[0]} isolates and {filtered_matrix.shape[1]} genetic variants")
+# filtered_matrix.to_pickle(os.path.join(out_dir, "filt_matrix.pkl"))
 
-# returns a tuple: current, peak memory in bytes 
-script_memory = tracemalloc.get_traced_memory()[1] / 1e9
-tracemalloc.stop()
-print(f"{script_memory} GB")
+# # returns a tuple: current, peak memory in bytes 
+# script_memory = tracemalloc.get_traced_memory()[1] / 1e9
+# tracemalloc.stop()
+# print(f"{script_memory} GB")
